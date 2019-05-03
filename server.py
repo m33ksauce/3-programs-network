1e23957a810b174c25cd941176af6afbb0552e2e
Chris Meek <m33ksauce@gmail.com>
2019-05-03 07:51:51 -0500

    It's a webserver now

--- server.py
+++ server.py
@@ -5,15 +5,22 @@ import socket
 HOST = '127.0.0.1'
 PORT = 1337
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
+while True:
     conn, addr = s.accept()
-    with conn:
-        print('connect by ', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(repr(data))
-            conn.sendall(data)
+    print('connect by ', addr)
+    # while True:
+    data = "<a href='http://google.com'>Google</a>"
+    pack = ("HTTP/1.1 200 OK\nContent-type: text/html\nConnection: close\nContent-length: " + str(len(data)) + "\n\n" + data).encode('ascii')
+    #data = data + ("Content-Length: " + (data.Length + 16)).encode('ascii')
+        # if not data:
+        #     break
+    print(repr(pack))
+    conn.send(pack)
+    # conn.close()
+s.close()
+
+def addHeader(header, data):
+    http_header = "HTTP/1.1"
